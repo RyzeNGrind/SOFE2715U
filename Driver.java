62d8946fa440415c5ddb1139852bb9f67ebc379d
Vidurshan Sribalasuhabiramam <100558257@oncampus.local>
2020-02-24 15:51:49 -0500

    Driver for RunTime Class

---
+++ Driver.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package customcode;
+
+/**
+ * @author 100558257
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Create object and initialize constructor
+		RunTime rTime = new RunTime();
+
+		//Sample test statement
+		System.out.println("Hello World!" +
+				"\n" + "This is a driver class testing methods from RunTime");
+
+		//Execute printTime() method
+		rTime.printTime();
+	}
+
+}
