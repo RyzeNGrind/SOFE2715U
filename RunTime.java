ce2215812e4c8c68a312a7ca07389e89f7f97336
Vidurshan Sribalasuhabiramam <100558257@oncampus.local>
2020-02-24 15:38:29 -0500

    Run Time calculation algorithm using get and set methods and a void class that doesn't return objects but uses them for references

---
+++ RunTime.java
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+package customcode;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 100558257
+ *
+ */
+public class RunTime {
+
+	/**
+	 *
+	 */
+
+	//private static long elapsedTime;
+	//private static long longElapsedTimeInSeconds;
+	private long startTime;
+	private long endTime;
+	//private static double doubleElapsedTimeInSeconds;
+	//private static long convert;
+
+	public RunTime() {
+		//System.out.println("RunTime constructor running");
+		setStartTime();
+	}
+	public void setStartTime() {
+		startTime = System.nanoTime();
+		//set startTime;
+	}
+	public boolean getFlagEndTime() {
+		endTime = System.nanoTime();
+		return true;
+		//set endTime and return true endTime flag
+	}
+	public void printTime() {
+		long elapsedTime = endTime - startTime;
+		double doubleElapsedTimeInSeconds = (double) elapsedTime/ 1_000_000_000;
+		long convert = TimeUnit.SECONDS.convert(elapsedTime,TimeUnit.NANOSECONDS);
+
+		System.out.println("\n" + "Runtime: " +
+				"\n" + elapsedTime + " nanoseconds" +
+				"\n" + doubleElapsedTimeInSeconds + " seconds" +
+				"\n" + convert + " seconds");
+		//System.out.println("RunTime calculation finished");
+	}
+}
